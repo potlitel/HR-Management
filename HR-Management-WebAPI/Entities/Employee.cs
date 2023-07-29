3a47a895621fd678ae29651bcaa393edbf1c1251
potlitel <potlitel@gmail.com>
2023-07-29 17:40:56 -0400

    Employee and Roles entities are created

---
+++ HR-Management-WebAPI/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Management_WebAPI.Entities
+{
+    public class Employee
+    {
+        public int employee_id { get; set; }
+        public string employee_name { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string personalAddress { get; set; }
+        public string phone { get; set; }
+        public DateTime workingStartingDate { get; set; }
+
+        public float startingSalary { get; set; }
+
+        public List<Roles> Roles { get; set; } = new List<Roles>();
+    }
+}
---
+++ HR-Management-WebAPI/Entities/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HR_Management_WebAPI.Entities
+{
+    public class Roles
+    {
+        public int role_id { get; set; }
+        public string rol_name { get; set; }
+    }
+}
