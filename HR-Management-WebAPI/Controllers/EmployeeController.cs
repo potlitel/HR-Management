31d6157c58f35d826879220c247743f61218269d
potlitel <potlitel@gmail.com>
2023-07-30 18:06:48 -0400

    The operation to delete employees is completed.

--- HR-Management-WebAPI/Controllers/EmployeeController.cs
+++ HR-Management-WebAPI/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using HR_Management_WebAPI.Contracts;
+using HR_Management_WebAPI.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -29,6 +30,28 @@ namespace HR_Management_WebAPI.Controllers
             {
                 var employees = await _employeesRepo.GetEmployees();
                 return Ok(employees);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+        /// <summary>
+        /// Api Get method to delete an existing employee.
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("delete/{employee_id}")]
+        public async Task<IActionResult> DeleteEmployee(int employee_id)
+        {
+            try
+            {
+                var roleFound = await _employeesRepo.GetEmployeeById(employee_id);
+                if (roleFound == null)
+                    return NotFound("No employee found matching the supplied identifier.");
+
+                CustomResponse model = await _employeesRepo.DeleteEmployee(employee_id);
+                return Ok(new { message = model.Message });
             }
             catch (Exception ex)
             {
--- HR-Management-WebAPI/Repository/EmployeeRepository.cs
+++ HR-Management-WebAPI/Repository/EmployeeRepository.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace HR_Management_WebAPI.Repository
@@ -19,19 +20,49 @@ namespace HR_Management_WebAPI.Repository
         {
             _context = context;
         }
-        public Task<CustomResponse> CreateRole(CreateRequest role)
+        public Task<CustomResponse> CreateEmployee(CreateRequest role)
         {
             throw new NotImplementedException();
         }
 
-        public Task<CustomResponse> DeleteRole(int role_id)
+        public async Task<CustomResponse> DeleteEmployee(int employee_id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var query = "Usp_HR_DelEmployee";
+                using (var connection = _context.CreateConnection())
+                {
+                    var parameters = new DynamicParameters();
+                    parameters.Add("employee_id", employee_id, DbType.Int32);
+                    parameters.Add("prp_mensaje", dbType: DbType.String, direction: ParameterDirection.Output, size: 250);
+
+                    await connection.ExecuteAsync(query, parameters, commandType: CommandType.StoredProcedure);
+
+                    var message = parameters.Get<string>("prp_mensaje");
+
+                    return (new CustomResponse
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Message = message,
+                    });
+                }
+            }
+            catch (Exception exp)
+            {
+                throw new Exception(exp.Message, exp);
+            }
         }
 
-        public Task<Employee> GetEmployeeById(int employee_id)
+        public async Task<Employee> GetEmployeeById(int employee_id)
         {
-            throw new NotImplementedException();
+            var procedure = "Usp_HR_SelByIdEmployee";
+            using (var connection = _context.CreateConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("employee_id", employee_id, DbType.Int32);
+                parameters.Add("prp_mensaje", dbType: DbType.String, direction: ParameterDirection.Output, size: 250);
+                return await connection.QuerySingleOrDefaultAsync<Employee>(procedure, parameters, null, commandType: CommandType.StoredProcedure);
+            }
         }
 
         public Task<Employee> GetEmployeeByName(string name)
@@ -50,7 +81,7 @@ namespace HR_Management_WebAPI.Repository
             }
         }
 
-        public Task<CustomResponse> UpdateRole(int role_id, CreateRequest role)
+        public Task<CustomResponse> UpdateEmployee(int role_id, CreateRequest role)
         {
             throw new NotImplementedException();
         }
