d2c269a98101bda4f6c17357eb2d0ec2050cdab6
potlitel <potlitel@gmail.com>
2023-07-31 11:04:10 -0400

    Minor change in HistoricalSalaries table.

--- HR-Management-Database/Tables/HistoricalSalaries.sql
+++ HR-Management-Database/Tables/HistoricalSalaries.sql
@@ -4,5 +4,6 @@
     [employee_id] INT NOT NULL FOREIGN KEY REFERENCES Employees(employee_id) ON DELETE CASCADE ON UPDATE CASCADE, 
     [salaries_increases] INT NOT NULL, 
     [increases_period] NVARCHAR(150) NOT NULL, 
-    [increases_date] DATETIME NOT NULL
+    [increases_date] DATETIME NOT NULL, 
+    [with_role] INT NOT NULL FOREIGN KEY REFERENCES Roles(role_id) ON DELETE CASCADE ON UPDATE CASCADE, 
 )
